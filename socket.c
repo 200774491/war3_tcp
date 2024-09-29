a196e32ecc5c66f1e54f2b064b6cb88b1fd4c653
花吃了阿九 <138980212+200774491@users.noreply.github.com>
2024-09-29 17:11:29 +0800

    socket.c

--- socket.c
+++ socket.c
@@ -3,28 +3,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include "socket_handler.h"
-
 #pragma comment(lib, "Ws2_32.lib")
-
-// 最大消息条目限制
 #define MAX_MESSAGES 1000
-
 SOCKET sock;
 char buffer[2048];
 char* messageQueue[MAX_MESSAGES];
 int messageCount = 0;
-
 // 接收数据并存储到消息队列
 void receive_data() {
-	// 设置套接字为非阻塞模式
-	u_long mode = 1; // 1表示非阻塞
+	u_long mode = 1;
 	ioctlsocket(sock, FIONBIO, &mode);
 	
 	int result = recv(sock, buffer, sizeof(buffer) - 1, 0);
 	if (result > 0) {
-		buffer[result] = '\0'; // 确保字符串结束
+		buffer[result] = '\0';
 		if (messageCount < MAX_MESSAGES) {
-			messageQueue[messageCount] = strdup(buffer); // 存储消息
+			messageQueue[messageCount] = strdup(buffer);
 			messageCount++;
 		}
 	} else if (result == 0) {
@@ -41,13 +35,13 @@ void receive_data() {
 __declspec(dllexport) int init_socket(const char* ip, int port) {
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-		return -1; // 初始化失败
+		return -1;
 	}
 	
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) {
 		WSACleanup();
-		return -1; // 创建套接字失败
+		return -1;
 	}
 	
 	struct sockaddr_in server;
@@ -58,39 +52,37 @@ __declspec(dllexport) int init_socket(const char* ip, int port) {
 	if (connect(sock, (struct sockaddr*)&server, sizeof(server)) != 0) {
 		closesocket(sock);
 		WSACleanup();
-		return -1; // 连接失败
+		return -1;
 	}
 	
-	return 0; // 成功
+	return 0;
 }
 
 // 发送数据
 __declspec(dllexport) int send_data(const char* data) {
 	int result = send(sock, data, strlen(data), 0);
-	return result == SOCKET_ERROR ? -1 : 0; // 发送失败返回-1
+	return result == SOCKET_ERROR ? -1 : 0; 
 }
 
 // 获取并移除最早的消息
 __declspec(dllexport) const char* get_message() {
 	receive_data();
 	if (messageCount > 0) {
-		const char* msg = messageQueue[0]; // 获取最早的消息
-		// 移除最早的消息
+		const char* msg = messageQueue[0];
 		for (int i = 1; i < messageCount; i++) {
 			messageQueue[i - 1] = messageQueue[i];
 		}
 		messageCount--;
-		return msg; // 返回最早的消息
+		return msg;
 	}
 	
-	return ""; // 没有消息，返回空字符串
+	return "";
 }
 
 // 关闭套接字
 __declspec(dllexport) void close_socket() {
 	closesocket(sock);
 	WSACleanup();
-	// 释放消息队列
 	for (int i = 0; i < messageCount; i++) {
 		free(messageQueue[i]);
 	}
